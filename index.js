6042eb117433924db8069518e55f9f05bb9b9fcc
lavi <lrajouri2002@gmail.com>
2024-08-27 15:12:14 +0530

    initial commit of learning curve

---
+++ index.js
@@ -0,0 +1,48 @@
+import { error } from "console";
+import ejs from "ejs"
+import express from "express";
+import https from "https";
+
+const port = 3000;
+const app = express();
+const API = "https://api.wheretheiss.at/v1/satellites";
+
+app.use(express.urlencoded({extended:1}));
+
+app.get("/", (req, res)=>{
+    const options = {
+        hostname : "bored-api.appbrewery.com",
+        path : "/random",
+        method : "GET"
+    };
+
+    const request = https.request(options, (response) => {
+        var data ="";
+        response.on("data", (chunk)=>{
+            data += chunk;
+        });
+
+        response.on("end", () => {
+            try{
+                const result = JSON.parse(data);
+                console.log(result);
+                res.render("index.ejs", {activity: result});
+            }catch{
+                //error handling
+                console.error("failed to parse response:", error.message);
+                res.status(500).send("failed to fetch activity. Please try again");
+            }
+        });
+    });
+
+    request.on("error", (error) => {
+        console.error("failed to make requests:", error.message );
+        res.status(500).send("failed to fetch activity, please try again");
+    });
+
+    request.end();
+});
+
+app.listen(port, ()=> {
+    console.log(`Server is running on PORT: ${port}`)
+})
