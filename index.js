a4b16fb7a1ea9bded5e426f9b6ee6da723cbdb5a
lavi <lrajouri2002@gmail.com>
2024-08-28 10:50:59 +0530

    add new ISS API

--- index.js
+++ index.js
@@ -5,27 +5,35 @@ import https from "https";
 
 const port = 3000;
 const app = express();
-const API = "https://api.wheretheiss.at/v1/satellites";
+
+var data ="";
+var result;
+const option_1 = {
+    hostname : "api.wheretheiss.at",
+    path : "/v1/satellites/25544",
+    method : "GET"
+};
+const option_2 = {
+    hostname : "bored-api.appbrewery.com",
+    path : "/random",
+    method : "GET"
+};
 
 app.use(express.urlencoded({extended:1}));
 
 app.get("/", (req, res)=>{
-    const options = {
-        hostname : "bored-api.appbrewery.com",
-        path : "/random",
-        method : "GET"
-    };
+    res.render("index.ejs", {activity: result});   
+});
 
-    const request = https.request(options, (response) => {
-        var data ="";
+app.post("/submit", (req, res) => {
+    const request = https.request(option_2, (response) => {  
         response.on("data", (chunk)=>{
             data += chunk;
         });
-
         response.on("end", () => {
             try{
-                const result = JSON.parse(data);
-                console.log(data);
+                result = JSON.parse(data);
+                console.log(data); 
                 res.render("index.ejs", {activity: result});
             }catch{
                 //error handling
@@ -34,12 +42,34 @@ app.get("/", (req, res)=>{
             }
         });
     });
-
     request.on("error", (error) => {
         console.error("failed to make requests:", error.message );
         res.status(500).send("failed to fetch activity, please try again");
     });
+    request.end();
+});
 
+app.post("/sub", (req, res)=>{
+    const request = https.request(option_1, (response) => {  
+        response.on("data", (chunk)=>{
+            data += chunk;
+        });
+        response.on("end", () => {
+            try{
+                result = JSON.parse(data);
+                console.log(data); 
+                res.render("index.ejs", {iss: result});
+            }catch{
+                //error handling
+                console.error("failed to parse response:", error.message);
+                res.status(500).send("failed to fetch activity. Please try again");
+            }
+        });
+    });
+    request.on("error", (error) => {
+        console.error("failed to make requests:", error.message );
+        res.status(500).send("failed to fetch activity, please try again");
+    });
     request.end();
 });
 
